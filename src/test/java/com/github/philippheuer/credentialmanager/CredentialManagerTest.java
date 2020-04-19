bc25e4332553690d0bae98f8065a8353649a190a
Waridley <waridley64@gmail.com>
2020-04-19 00:24:24 -0500

    Fix CredentialManagerTest

--- src/test/java/com/github/philippheuer/credentialmanager/CredentialManagerTest.java
+++ src/test/java/com/github/philippheuer/credentialmanager/CredentialManagerTest.java
@@ -1,19 +1,24 @@
 package com.github.philippheuer.credentialmanager;
 
 import com.github.philippheuer.credentialmanager.domain.Credential;
+import com.github.philippheuer.credentialmanager.domain.IdentityProvider;
 import com.github.philippheuer.credentialmanager.domain.OAuth2Credential;
-import com.github.philippheuer.credentialmanager.identityprovider.TwitchIdentityProvider;
+import com.github.philippheuer.credentialmanager.identityprovider.OAuth2IdentityProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 @Tag("unittest")
 public class CredentialManagerTest {
-
+    
+    static String providerName = "testidp";
+    
     /**
      * Test Builder
      */
@@ -24,7 +29,7 @@ public class CredentialManagerTest {
         CredentialManager credentialManager = CredentialManagerBuilder.builder().build();
 
         // asserts
-        assertTrue(credentialManager.getStorageBackend() != null, "Storage Backend not registered!");
+        assertNotNull(credentialManager.getStorageBackend(), "Storage Backend not registered!");
     }
 
     /**
@@ -35,13 +40,22 @@ public class CredentialManagerTest {
     public void saveCredential() {
         // build
         CredentialManager credentialManager = CredentialManagerBuilder.builder().build();
-
+        
+        IdentityProvider provider = new OAuth2IdentityProvider(providerName, "oauth2", null, null, null, null, null) {
+            @Override
+            public Optional<OAuth2Credential> getAdditionalCredentialInformation(OAuth2Credential credential) {
+                return Optional.of(credential);
+            }
+        };
+        credentialManager.registerIdentityProvider(provider);
+        
         // add credential
-        Credential credential = new OAuth2Credential("twitch", "tokenHere");
-        credentialManager.addCredential("twitch", credential);
+        Credential credential = new OAuth2Credential(providerName, "tokenHere");
+        credentialManager.addCredential(providerName, credential);
 
         // asserts
-        assertTrue(credentialManager.getCredentials().size() == 1, "Credential wasn't added!");
+        assertEquals(1, credentialManager.getCredentials().size(), "Credential wasn't added!");
     }
 
 }
+
